20bfd53d5d18c0dbf2da6cbdc0f5a80f2f40fdd4
Rigwarl <Avips@mail.ru>
2016-03-23 16:33:56 +0300

    add fire

--- app/js/es6/elements/hero.js
+++ app/js/es6/elements/hero.js
@@ -13,9 +13,27 @@ export default class Hero extends Element {
   constructor(args) {
     args.body = 'playerShip1_orange';
     super(args);
+    this.createFire();
     this.createProps();
     this.el.addEventListener('tick', () => this.tick());
     collisions.add(this);
+  }
+  createFire() {
+    this.ss.getAnimation('fire13').next = 'fire12';
+    this.ss.getAnimation('fire12').next = 'fire13';
+    this.ss.getAnimation('fire17').next = 'fire13';
+
+    this.fireLeft = new createjs.Sprite(this.ss, 'fire13').set({
+      x: 17,
+      y: 60,
+      scaleY: 0,
+    });
+    this.fireRight = new createjs.Sprite(this.ss, 'fire13').set({
+      x: 68,
+      y: 60,
+      scaleY: 0,
+    });
+    this.el.addChildAt(this.fireLeft, this.fireRight, 0);
   }
   createProps() {
     this.rotation = 0;
@@ -28,6 +46,25 @@ export default class Hero extends Element {
   tick() {
     this.handleActions();
     this.move();
+    this.animateFire();
+  }
+  animateFire() {
+    let leftTo = 0;
+    let rightTo = 0;
+
+    if (this.thrust === -1) {
+      leftTo = 1;
+      rightTo = 1;
+
+      if (this.heading === -1) leftTo = 0.75;
+      else if (this.heading === 1) rightTo = 0.75;
+    } else {
+      if (this.heading === -1) rightTo = 0.65;
+      else if (this.heading === 1) leftTo = 0.65;
+    }
+
+    createjs.Tween.get(this.fireRight, { override: true }).to({ scaleY: rightTo }, 300);
+    createjs.Tween.get(this.fireLeft, { override: true }).to({ scaleY: leftTo }, 300);
   }
   move() {
     this.rotation += this.vRot * CONFIG.rotSpeed;
@@ -50,6 +87,7 @@ export default class Hero extends Element {
     this.vY = this.vY * CONFIG.inertia;
   }
   handleActions() {
+    this.oldThrust = this.thrust;
     this.thrust = 0;
     this.heading = 0;
 
