12bfc329cafcfa23ab8e97407183386aef8c9ed2
Rigwarl <Avips@mail.ru>
2016-03-29 11:16:48 +0300

    make check pair and checkFrameHit public

--- app/js/es6/collisions.js
+++ app/js/es6/collisions.js
@@ -15,37 +15,52 @@ export default {
   },
   process(lb) {
     items.forEach(obj1 => {
-      checkFrameHit(obj1, lb);
+      if (obj1.constructor.name !== 'Laser') {
+        const frameHit = this.checkFrameHit(obj1, lb);
+
+        if (frameHit) {
+          if (frameHit.x) obj1.vX = -obj1.vX * 0.85;
+          else if (frameHit.y) obj1.vY = -obj1.vY * 0.85;
+        }
+      }
+
       items.forEach(obj2 => {
-        checkPair(obj1, obj2);
+        if (this.checkPair(obj1, obj2)) {
+          setAction(obj1, obj2);
+        }
       });
     });
 
     actions.forEach(item => item.obj1[item.action](item.obj2));
     actions.clear();
   },
+  checkPair(obj1, obj2) {
+    if (obj1 !== obj2) {
+      if (obj1.radius + obj2.radius > getDistance(obj1, obj2)) {
+        return true;
+      }
+    }
+    return false;
+  },
+  checkFrameHit(obj, lb) {
+    const result = {};
+
+    if ((obj.el.x - obj.radius < 0 && obj.vX < 0) ||
+        (obj.el.x + obj.radius > lb.width && obj.vX > 0)) {
+      result.x = true;
+    }
+    if ((obj.el.y - obj.radius < 0 && obj.vY < 0) ||
+        (obj.el.y + obj.radius > lb.height && obj.vY > 0)) {
+      result.y = true;
+    }
+
+    return result;
+  },
 };
 
-function checkPair(obj1, obj2) {
-  if (obj1 !== obj2) {
-    if (obj1.radius + obj2.radius > getDistance(obj1, obj2)) {
-      const action = collideTable[obj1.constructor.name][obj2.constructor.name];
-      if (action) actions.add({ obj1, action, obj2 });
-    }
-  }
-}
-
-function checkFrameHit(obj, lb) {
-  if (obj.constructor.name === 'Laser') return;
-
-  if ((obj.el.x - obj.radius < 0 && obj.vX < 0) ||
-      (obj.el.x + obj.radius > lb.width && obj.vX > 0)) {
-    obj.vX = -obj.vX * 0.85;
-  }
-  if ((obj.el.y - obj.radius < 0 && obj.vY < 0) ||
-      (obj.el.y + obj.radius > lb.height && obj.vY > 0)) {
-    obj.vY = -obj.vY * 0.85;
-  }
+function setAction(obj1, obj2) {
+  const action = collideTable[obj1.constructor.name][obj2.constructor.name];
+  if (action) actions.add({ obj1, action, obj2 });
 }
 
 function getDistance(obj1, obj2) {
